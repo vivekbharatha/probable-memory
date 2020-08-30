baac5bde90a5234137cc1769a31a31a10c30cda6
vivekbharatha <vivek.bharatha@gmail.com>
2020-08-31 01:39:40 +0530

    adds authorization and CRD of posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,68 @@
 const Post = require('../../models/Post');
 
+const checkAuth = require('../../util/checkAuth');
+const { AuthenticationError } = require('apollo-server');
+
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (error) {
         throw new Error(error);
       }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = Post.findById(postId);
+        if (!post) {
+          throw new Error('Post not found');
+        }
+
+        return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        body,
+        username: user.username,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      const post = await newPost.save();
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (!post) {
+          throw new Error('Invalid postId');
+        }
+
+        if (!post.username === user.username) {
+          throw new AuthenticationError('Not allowed');
+        }
+
+        await post.remove();
+        return 'Post delete';
+      } catch (error) {
+        throw new Error(error);
+      }
+
+      console.log(newPost);
+
+      const post = await newPost.save();
+      return post;
     },
   },
 };
