107339d64b3c33b8d62affc76cc778c5a1100bd2
vivekbharatha <vivek.bharatha@gmail.com>
2020-09-04 02:47:12 +0530

    adds env variable to swap BASE_URL during build for client

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,7 +6,9 @@ import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
-const httpLink = createHttpLink({ uri: 'http://localhost:5000' });
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+
+const httpLink = createHttpLink({ uri: API_BASE_URL });
 
 const authLink = setContext(() => {
   const token = localStorage.getItem('token');
