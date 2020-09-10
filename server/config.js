b39d754bf87a232a38c3cec0bbbf9bd10078be5b
vivekbharatha <vivek.bharatha@gmail.com>
2020-09-10 18:15:13 +0530

    clean up of server code

--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGO_DB_URL: process.env.MONGO_DB_URL || 'mongodb://localhost:27017/probable-memory',
-  SECRET_KEY: process.env.SECRET_KEY || "It'sNotSecretForNow",
-};
--- server/docker-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-    -it \
-    --rm \
-    -v ${PWD}:/app \
-    -v /app/node_modules \
-    -p 5000:5000 \
-    probable-memory/server:latest
